6a6cbb8423e5f1108bf0c404c9b68d1fbed2026d
RedMission <dzx0820@qq.com>
2023-05-04 09:32:05 +0800

    跑同济

--- know_datasets.py
+++ know_datasets.py
@@ -11,13 +11,13 @@ from utils.parser import get_dagan_args
 if __name__ == '__main__':
     # 加载数据
     # raw_data = np.load("datasets/omniglot_data.npy")
-    raw_data = np.load("datasets/IITDdata_right_3.npy",allow_pickle=True)
+    raw_data = np.load("F:\jupyter_notebook\DAGAN\datasets\Tongji_session2.npy",allow_pickle=True)
     print("数据集类型：",type(raw_data))
     print("数据集形状：",raw_data.shape)
     # print("一类数据类型：",type(raw_data[0]))
     # print("一类数据形状：",raw_data[0].shape)
     #
-    print("一张图片：",(raw_data[0][2]))
-    # print("一张图片的形状：",raw_data[0][2].shape)
+    # print("一张图片：",(raw_data[0][2]))
+    print("一张图片的形状：",raw_data[0][2].shape)
 
     # 解析npy保存成图片？
