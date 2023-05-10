c1b263bc2fe2b71eb0acc686c2d4cd9cad854cfe
RedMission <dzx0820@qq.com>
2023-05-10 10:36:13 +0800

    MCCGAN(PSA*2)

--- discriminator.py
+++ discriminator.py
@@ -187,7 +187,7 @@ class _PSAEncoderBlock(nn.Module):
         total_channels += out_channels  # 通道数在增加
 
         self.add_module(
-            "conv%d" % num_layers+1, # 第num_layers+1层
+            "conv%d" % str(num_layers+1), # 第num_layers+1层
             _conv2d(
                 in_channels=total_channels,
                 out_channels=out_channels,
